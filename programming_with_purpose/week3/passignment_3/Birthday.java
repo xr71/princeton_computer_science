2590c6d97a6a175eea615fb5a280f0e279512339
Xu Ren <musicalxu@hotmail.com>
2020-04-20 12:04:35 +0800

    birthday simulation

--- programming_with_purpose/week3/passignment_3/Birthday.java
+++ programming_with_purpose/week3/passignment_3/Birthday.java
@@ -5,24 +5,36 @@ public class Birthday {
         int days = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
 
-        int person = 0;
+        int person = 1;
         int counter = 0;
-        boolean[] birthdays = new boolean[366];
+        boolean[] birthdays = new boolean[days];
+        int exactperson = 0;
+        double frac = 0;
 
-        for (int i = 0; i < 24; i++) {
 
-            int new_person = (int) (Math.random() * days);
+        while (frac < 0.5) {
+            frac = 0.0;
+            counter = 0;
+            exactperson = 0;
 
-            System.out.println(new_person);
-            if (birthdays[new_person]) {
-                counter++;
-            } else {
-                birthdays[new_person] = true;
+            for (int i = 0; i < trials; i++) {
+                birthdays = new boolean[days];
+
+                for (int p = 0; p < person; p++) {
+                    int new_person = (int) (Math.random() * days);
+
+                    if (birthdays[new_person]) {
+                        counter++;
+                        if (p == person-1) exactperson++;
+                        break;
+                    } else {
+                        birthdays[new_person] = true;
+                    }
+                }
             }
-
-            System.out.println(counter);
-
+            frac = (double) counter / (double) trials;
+            System.out.println(person + "\t" + exactperson + "\t" + frac);
+            person++;
         }
-
     }
 }
